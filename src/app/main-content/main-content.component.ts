d7657b7a7052553459e1868367262474d0cdc0a2
Ismail Baris <ismail.93.baris@gmail.com>
2025-01-29 03:06:59 +0100

    contact section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,11 +5,12 @@ import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { ReferencesComponent } from './references/references.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { TitleComponent } from './title/title.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, ContactMeComponent, MyProjectsComponent, ReferencesComponent,SkillSetComponent,TitleComponent],
+  imports: [AboutMeComponent, ContactMeComponent, MyProjectsComponent, ReferencesComponent,SkillSetComponent,TitleComponent,FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
