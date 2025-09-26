0f0e9d953e522de3132c1fbc3209029c0f957e5c
Ismail Baris <ismail.93.baris@gmail.com>
2025-09-26 13:44:40 +0200

    change: project-modal -- added individual project overviews

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
+import { ProjectModalComponent } from '../../shared/project-modal/project-modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectModalComponent],
   templateUrl: './my-projects.component.html',
-  styleUrl: './my-projects.component.scss'
+  styleUrls: ['./my-projects.component.scss']
 })
 export class MyProjectsComponent {
   projects = [
     {
       title: 'Join',
-      description: 'Task manager inspired by the Kanban System...',
-      techStack: ['CSS', 'HTML', 'Firebase', 'Angular', 'TypeScript'],
-      github: 'https://github.com/dein-repo',
-      live: 'https://dein-live-link',
-      image: 'assets/img/my_projects/join.png'
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      techStack: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      github: 'https://github.com/Ismail-B/join',
+      live: 'https://join-live-link.com',
+      image: 'assets/img/my_projects/join_preview.png'
     },
     {
-      title: 'Next App',
-      description: 'Another awesome project...',
-      techStack: ['React', 'NodeJS'],
-      github: '#',
-      live: '#',
-      image: 'assets/img/my_projects/next.png'
+      title: 'Shadow Shinobi',
+      description: 'Jump & run ninja game with fun mechanics and pixel art style.',
+      techStack: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/Ismail-B/Shadow-Shinobi',
+      live: 'https://shadow-shinobi-live.com',
+      image: 'assets/img/my_projects/shadow_shinobi_preview.png'
+    },
+    {
+      title: 'Pokedex',
+      description: 'Simple app to browse Pokémon using an API.',
+      techStack: ['HTML', 'CSS', 'API', 'JavaScript'],
+      github: 'https://github.com/Ismail-B/Pokedex',
+      live: 'https://pokedex-live.com',
+      image: 'assets/img/my_projects/pokedex_preview.png'
     }
   ];
 
--- src/app/shared/project-modal/project-modal.component.ts
+++ src/app/shared/project-modal/project-modal.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-modal.component.html',
-  styleUrl: './project-modal.component.scss'
+  styleUrls: ['./project-modal.component.scss']
 })
 export class ProjectModalComponent {
+  @Input() project: any;
+  @Input() projectList: any[] = [];
+  @Input() currentIndex!: number;
+  @Output() close = new EventEmitter<void>();
+  @Output() updateIndex = new EventEmitter<number>();
 
+  onBackdropClick() {
+    this.close.emit();
+  }
+
+  nextProject() {
+    const nextIndex = (this.currentIndex + 1) % this.projectList.length;
+    this.updateIndex.emit(nextIndex);
+  }
 }
