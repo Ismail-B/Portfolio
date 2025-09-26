cbcb9dd0a498d48d93ec7dcab09cb6ab1da7b516
Ismail Baris <ismail.93.baris@gmail.com>
2025-09-26 04:04:07 +0200

    change: title -- mailTo , generated new c project-modal

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
+  projects = [
+    {
+      title: 'Join',
+      description: 'Task manager inspired by the Kanban System...',
+      techStack: ['CSS', 'HTML', 'Firebase', 'Angular', 'TypeScript'],
+      github: 'https://github.com/dein-repo',
+      live: 'https://dein-live-link',
+      image: 'assets/img/my_projects/join.png'
+    },
+    {
+      title: 'Next App',
+      description: 'Another awesome project...',
+      techStack: ['React', 'NodeJS'],
+      github: '#',
+      live: '#',
+      image: 'assets/img/my_projects/next.png'
+    }
+  ];
 
+  selectedProjectIndex: number | null = null;
+
+  openModal(index: number) {
+    this.selectedProjectIndex = index;
+  }
+
+  closeModal() {
+    this.selectedProjectIndex = null;
+  }
+
+  updateProjectIndex(newIndex: number) {
+    this.selectedProjectIndex = newIndex;
+  }
 }
