2346b60202898f4fe0c9ef2ba51293118f93e106
Ismail Baris <ismail.93.baris@gmail.com>
2025-09-29 01:35:13 +0200

    changed: angular.json

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../shared/navbar/navbar.component'; 
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
